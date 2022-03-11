e6b09b0d9b051a64ef4a7eb80d77254202fe6f6e
Luis <luisjin9@gmail.com>
2022-03-11 10:31:31 +0100

    Done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class UserController extends Controller
-{
+class UserController extends Controller {
     //Se emulan datos de una base de datos con el siguiente array:
     public $users =  [
         [
@@ -22,21 +21,18 @@ class UserController extends Controller
         ],
     ];
 
-    public function allUsers()
-    {
+    public function allUsers() {
         //En vez de utilizar el modelo, se devuelve el array $users emulando 
         //una llamada a la base de datos, pero en un caso real se haría una llamada
         //al método ::all() del modelo User
 
         return $this->users;
     }
-    public function getUserById($id)
-    {
+    public function getUserById($id) {
         //Emulamos el método ::find($id) del modelo User
         return $this->users[$id - 1];
     }
-    public function postSomething(Request $request)
-    {
+    public function postSomething(Request $request) {
         //En el argumento $request, se almacenan todos los datos enviados por el método post
         // Ej. se pasa por post el parámetro "edad" con valor 21. $request->edad será igual a 21.
 
@@ -55,14 +51,36 @@ class UserController extends Controller
             //probar a devolver también el campo apellidos
         ]);
     }
-    public function postOtherThing(Request $request)
-    {
+    public function postOtherThing(Request $request) {
         // Crear una función en la que si se pasa el parámetro "premium" a true en el $request, 
         // devuelva un código de estado "OK" y un mensaje "el usuario es premium". 
-        // En caso de que se le pase premium false, devolver un código de estado "KO" y mensaje "error: usuario no premium"
+        // En caso de que se le pase premium false, devolver un código de estado "KO" y
+        // mensaje "error: usuario no premium"
+
+        if ($request->premium) {
+            return response()->json([
+                "status" => "OK",
+                "mensaje" => 'El usuario es premium'
+            ]);
+        } else {
+            return response()->json([
+                "status" => "KO",
+                "mensaje" => 'error: usuario no es premium'
+            ]);
+        }
     }
-    public function deleteSomething(Request $request)
-    {
+    public function deleteSomething(Request $request) {
         // igual que la función anterior pero con un método de tipo delete 
+        if ($request->premium) {
+            return response()->json([
+                "status" => "OK",
+                "mensaje" => 'El usuario es premium'
+            ]);
+        } else {
+            return response()->json([
+                "status" => "KO",
+                "mensaje" => 'error: usuario no es premium'
+            ]);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/users', [UserController::class, 'allUsers']);
 Route::get('/users/{id}', [UserController::class, 'getUserById']);
 Route::post('/something', [UserController::class, 'postSomething']);
+Route::post('/otherThing', [UserController::class, 'postOtherThing']);
+Route::delte('/something', [UserController::class, 'deleteSomething']);
